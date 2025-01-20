0f82e6d140f728ebffcb1a7be15e2a9d64656f6d
shachard123 <shachardahan10@gmail.com>
2025-01-20 10:10:04 +0200

    Feat(ClientManager,User): switched from user map to user class

---
+++ src/main/kotlin/com/example/models/User.kt
@@ -0,0 +1,6 @@
+package com.example.models
+
+data class User(
+    val username: String,
+    var password: String
+)
--- src/main/kotlin/com/example/server/ClientManager.kt
+++ src/main/kotlin/com/example/server/ClientManager.kt
@@ -1,29 +1,32 @@
 package com.example.server
 
+import com.example.models.User
 import io.ktor.utils.io.*
 
 class ClientManager {
     private val connectedClients = mutableMapOf<String, ByteWriteChannel>()
 
-    private val users = mutableMapOf(
-        "shachar5" to "myStrongPass123",
-        "shachar2" to "password"
+    private val users = mutableListOf(
+        User("shachar5", "myStrongPass123"),
+        User("shachar2", "password")
     )
 
     fun addUser(username: String, password: String) {
-        users[username] = password
+        users.add(User(username, password))
     }
 
     fun removeUser(username: String) {
-        users.remove(username)
+        users.removeIf { it.username == username }
     }
 
-    fun checkCredentials(username: String, password: String): Boolean {
-        return users[username] == password
+    fun checkCredentials(username: String, attemptedPassword: String): Boolean {
+        //check if password of username equals attemptedPassword
+        val realPassword = users.find { it.username == username }?.password
+        return realPassword == attemptedPassword
     }
 
     fun checkUserExists(username: String): Boolean {
-        return users.containsKey(username)
+        return users.any { it.username == username }
     }
 
 
