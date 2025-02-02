5ee11a3586c4e57da64168801b50460b2798c449
shachard123 <shachardahan10@gmail.com>
2025-02-02 13:44:04 +0200

    refactor(chatClient,RequestManager): seperated request response matching to requestManager

--- src/main/kotlin/com/example/client/ChatClient.kt
+++ src/main/kotlin/com/example/client/ChatClient.kt
@@ -19,9 +19,6 @@ import org.jetbrains.annotations.VisibleForTesting
 
 class ChatClient(private val host: String, private val port: Int, private val timeout: Long) {
 
-    //store responses by request id to be able to match them
-    private val pendingRequests = mutableMapOf<String, CompletableDeferred<ServerResponse>>()
-    //incoming chat messages
     private val incomingChatMessages = Channel<ServerResponse.ChatMessage>()
 
     private val clientScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
@@ -35,17 +32,19 @@ class ChatClient(private val host: String, private val port: Int, private val ti
         val sendChannel = clientSocket.openWriteChannel(autoFlush = true)
         val receiveChannel = clientSocket.openReadChannel()
 
+        val requestManager = RequestManager(sendChannel, timeout)
+
         // Read messages from the server and pass them to the channel
         clientScope.launch {
-            listenForServerResponses(receiveChannel)
+            listenForServerResponses(receiveChannel, requestManager)
         }
 
         // login and show chat
         clientScope.launch {
-            runMainMenuLoop(sendChannel)
+            runMainMenuLoop(requestManager)
 
-            if(isLoggedIn) {
-                requestJoinRoom(sendChannel)
+            if (isLoggedIn) {
+                requestJoinRoom(requestManager)
                 startChatSession(sendChannel)
             }
 
@@ -56,67 +55,51 @@ class ChatClient(private val host: String, private val port: Int, private val ti
         clientScope.coroutineContext.job.join()
     }
 
-    private suspend fun listenForServerResponses(receiveChannel : ByteReadChannel) {
-        receiveChannel
-            .asFlow()
+    private suspend fun listenForServerResponses(
+        receiveChannel: ByteReadChannel,
+        requestManager: RequestManager
+    ) {
+        receiveChannel.asFlow()
             .map { Json.decodeFromString<ServerResponse>(it) }
             .collect { serverResponse ->
-                serverResponse.handleServerResponse(receiveChannel)
+                when (serverResponse) {
+                    is ServerResponse.ChatMessage -> incomingChatMessages.send(serverResponse)
+                    is ServerResponse.Success,
+                    is ServerResponse.Error -> requestManager.completeResponse(serverResponse)
+                }
             }
     }
 
-    private suspend fun ServerResponse.handleServerResponse(receiveChannel: ByteReadChannel) {
-        when (this) {
-            is ServerResponse.ChatMessage -> {
-                //send chat message
-                incomingChatMessages.send(this)
-            }
-
-            is ServerResponse.Success,
-            is ServerResponse.Error -> {
-                //if response to a request, complete the request
-                pendingRequests[id]?.complete(this)
-                pendingRequests.remove(id)
-            }
-        }
-    }
-
-    private suspend fun runMainMenuLoop(sendChannel : ByteWriteChannel) {
+    private suspend fun runMainMenuLoop(requestManager: RequestManager) {
         val menuString = """
-        ============================================
-                   Shachar's Chat App
-        ============================================
-        
-        [${MenuOption.LOGIN.code}] Login
-        [${MenuOption.SIGN_UP.code}] Sign Up
-        [${MenuOption.EXIT.code}] Exit
-        
-        ============================================
-        Please select an option:
-        
+            ============================================
+                       Shachar's Chat App
+            ============================================
+            
+            [${MenuOption.LOGIN.code}] Login
+            [${MenuOption.SIGN_UP.code}] Sign Up
+            [${MenuOption.EXIT.code}] Exit
+            
+            ============================================
+            Please select an option:
         """.trimIndent()
 
         while (!isLoggedIn && clientScope.isActive) {
-            val option = getInput(menuString)
-            when (option) {
-                "${MenuOption.LOGIN.code}" -> requestLogin(sendChannel)
-
-                "${MenuOption.SIGN_UP.code}" -> requestSignUp(sendChannel)
-
+            when (val option = getInput(menuString)) {
+                "${MenuOption.LOGIN.code}" -> requestLogin(requestManager)
+                "${MenuOption.SIGN_UP.code}" -> requestSignUp(requestManager)
                 "${MenuOption.EXIT.code}" -> exitProcess(0)
-
                 else -> println("Invalid option, please try again.")
             }
         }
     }
 
-    private suspend fun requestJoinRoom(sendChannel: ByteWriteChannel) {
+    private suspend fun requestJoinRoom(requestManager: RequestManager) {
         val room = getInput("Please enter the name of the chat room you want to join: ")
         val requestId = generateId()
         val request = ClientRequest.JoinRoom(requestId, room)
 
-        val response = request.sendAndAwaitResponse(sendChannel)
-        when (response) {
+        when (val response = requestManager.sendRequest(request)) {
             is ServerResponse.Success -> println("Joined room: $room")
             is ServerResponse.Error -> println("Failed to join room: ${response.message}")
             else -> println("Unexpected response to join room.")
@@ -124,51 +107,39 @@ class ChatClient(private val host: String, private val port: Int, private val ti
     }
 
     // send login request to server and handles the response
-    suspend fun requestLogin(sendChannel: ByteWriteChannel) {
-        val enteredUsername = getInput("Username: ")
-        val enteredPassword = getInput("Password: ")
+    private suspend fun requestLogin(requestManager: RequestManager) {
+        val username = getInput("Username: ")
+        val password = getInput("Password: ")
         val requestId = generateId()
-
-        val request = ClientRequest.Login(requestId, enteredUsername, enteredPassword)
-        val response = request.sendAndAwaitResponse(sendChannel)
-
-        when (response) {
+        val request = ClientRequest.Login(requestId, username, password)
+        when (val response = requestManager.sendRequest(request)) {
             is ServerResponse.Success -> {
                 isLoggedIn = true
                 println("Login successful: ${response.message}")
             }
-            is ServerResponse.Error -> {
-                println("Login failed: ${response.message}")
-            }
-            else -> {
-                println("Unexpected response to login.")
-            }
+
+            is ServerResponse.Error -> println("Login failed: ${response.message}")
+            else -> println("Unexpected response to login.")
         }
     }
 
     // send signup request to server and handles the response
-    private suspend fun requestSignUp(
-        sendChannel: ByteWriteChannel,
-    ) {
+    private suspend fun requestSignUp(requestManager: RequestManager) {
         var isValidUsername = false
-
-        // Loop until a valid username is found
         while (!isValidUsername && clientScope.isActive) {
             val username = getInput("Please enter a username: ")
             val password = getInput("Please enter a password: ")
             val requestId = generateId()
             val request = ClientRequest.SignUp(requestId, username, password)
-            try {
-                val response = request.sendAndAwaitResponse(sendChannel)
-                if (response is ServerResponse.Success) {
+            when (val response = requestManager.sendRequest(request)) {
+                is ServerResponse.Success -> {
                     isValidUsername = true
                     println(response.message)
                     println("Please log in with your new account.")
-                } else if (response is ServerResponse.Error) {
-                    println(response.message)
                 }
-            } catch (e: TimeoutCancellationException) {
-                println("Signup failed: Server did not respond in time.")
+
+                is ServerResponse.Error -> println(response.message)
+                else -> println("Unexpected response to sign up.")
             }
         }
     }
@@ -182,7 +153,8 @@ class ChatClient(private val host: String, private val port: Int, private val ti
                 println("Welcome to the chat! Type a message and press Enter to send.")
                 while (isActive) {
                     val serverMessage = incomingChatMessages.receive()
-                    val formattedMessage = formatChatMessage(serverMessage.sender, serverMessage.message, serverMessage.room)
+                    val formattedMessage =
+                        formatChatMessage(serverMessage.sender, serverMessage.message, serverMessage.room)
                     println(formattedMessage)
                 }
             }
@@ -195,21 +167,6 @@ class ChatClient(private val host: String, private val port: Int, private val ti
                     request.send(sendChannel)
                 }
             }
-        }
-    }
-
-
-    private suspend fun ClientRequest.sendAndAwaitResponse(sendChannel: ByteWriteChannel): ServerResponse {
-        //create pending response
-        val pendingResponse = CompletableDeferred<ServerResponse>()
-        pendingRequests[id] = pendingResponse
-
-        //send the request
-        this.send(sendChannel)
-
-        //finish when the response is received
-        return withTimeout(timeout) {
-            pendingResponse.await()
         }
     }
 
